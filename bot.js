7b943e0a87a468b067b02159b1c694c591ee7c68
Burdette <tburdette@johnsonoutdoors.com>
2017-03-20 11:20:17 -0400

    Adding discord.js version check and diagnostics.

--- bot.js
+++ bot.js
@@ -1,11 +1,19 @@
-const Discord = require('discord.js');
+try {
+	var Discord = require('discord.js');
+} catch (e) {
+	console.log(e.stack);
+	console.log(process.version);
+	console.log("Looks like your libs are missing or wrong. Please run npm install and check for errors.");
+	process.exit();
+}
+console.log("Starting Chocolina.\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version);
 
 const bot = new Discord.Client();
 
 const token = require('./auth.json').token;
 
 bot.on('ready', () => {
-	console.log('I am ready!');
+	console.log("I'm Chocolina, super time-traveling salesgirl!");
 });
 
 bot.on('message', message => {
