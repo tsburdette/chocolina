7ad2a10ceb2b27fc589e8f7f3a6cf62016ad0da7
Burdette <tburdette@johnsonoutdoors.com>
2017-03-20 17:22:23 -0400

    more diagnostic work. Started command parsing infrastructure.

--- bot.js
+++ bot.js
@@ -8,20 +8,52 @@ try {
 }
 console.log("Starting Chocolina.\nNode version: " + process.version + "\nDiscord.js version: " + Discord.version);
 
-const bot = new Discord.Client();
+try {
+	var token = require('./auth.json').token;
+} catch (e) {
+	console.log("auth.json either missing or wrong. Please ensure auth.json has a token.");
+	process.exit();
+}
 
-const token = require('./auth.json').token;
+var commands = {
+	"ping": {
+		description: "responds with 'pong'. Used as a heartbeat command."
+		process: function(bot, msg) {
+			msg.channel.sendMessage(msg.author + " pong!");
+		}
+	}
+}
 
-bot.on('ready', () => {
+var bot = new Discord.Client();
+
+bot.on("ready", function () => {
 	console.log("I'm Chocolina, super time-traveling salesgirl!");
+	// TODO: Make command prefix arbitrary, help response, other servers?
+	// bot.user.setGame("Try \'!help\'.")
 });
 
-bot.on('message', message => {
-	if (message.content === 'ping') {
-		message.channel.sendMessage('pong');
+bot.on("disconnected", function () {
+	console.log("Disconnected!");
+	process.exit(1);
+});
+
+function parseCommand(msg) {
+	// UNFINISHED OH GOD OH GOD OH GOD
+	// TODO: Make command prefix arbitrary.
+	if ((msg.author.id != bot.user.id) && (msg.content.startsWith("!"))) {
+		console.log(msg.author + " requested " + msg.content);
+		// TODO: Make command prefix arbitrary
+		var cmdTxt = msg.content.split(" ")[0].substring("!".length);
+		var suffix = msg.content.substring(cmdTxt.length + "!".length + 1);
 	}
-	if (message.content === 'who?') {
-		message.channel.sendFile('images\\chocobocolina.gif');
+}
+
+bot.on('message', (msg) => {
+	if (msg.content === 'ping') {
+		msg.channel.sendMessage('pong');
+	}
+	if (msg.content === 'who?') {
+		msg.channel.sendFile('images\\chocobocolina.gif');
 	}
 });
 
