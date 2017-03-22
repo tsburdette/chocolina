d7671e9c0ef12ec0ef652eefd9baff925aa204dd
Burdette <tburdette@johnsonoutdoors.com>
2017-03-22 14:29:00 -0400

    now evaluates arbitrary command text via ! prefix

--- bot.js
+++ bot.js
@@ -17,8 +17,8 @@ try {
 
 var commands = {
 	"ping": {
-		description: "responds with 'pong'. Used as a heartbeat command."
-		process: function(bot, msg) {
+		description: "responds with 'pong'. Used as a heartbeat command.",
+		process: (bot, msg, suffix) => {
 			msg.channel.sendMessage(msg.author + " pong!");
 		}
 	}
@@ -26,34 +26,37 @@ var commands = {
 
 var bot = new Discord.Client();
 
-bot.on("ready", function () => {
+bot.on("ready", () => {
 	console.log("I'm Chocolina, super time-traveling salesgirl!");
 	// TODO: Make command prefix arbitrary, help response, other servers?
 	// bot.user.setGame("Try \'!help\'.")
 });
 
-bot.on("disconnected", function () {
+bot.on("disconnected", () => {
 	console.log("Disconnected!");
 	process.exit(1);
 });
 
 function parseCommand(msg) {
-	// UNFINISHED OH GOD OH GOD OH GOD
 	// TODO: Make command prefix arbitrary.
 	if ((msg.author.id != bot.user.id) && (msg.content.startsWith("!"))) {
 		console.log(msg.author + " requested " + msg.content);
 		// TODO: Make command prefix arbitrary
 		var cmdTxt = msg.content.split(" ")[0].substring("!".length);
 		var suffix = msg.content.substring(cmdTxt.length + "!".length + 1);
+		try {
+			commands[cmdTxt].process(bot, msg, suffix);
+		} catch(e) {
+			msg.channel.sendMessage("I don't know how to " + cmdTxt);
+		}
 	}
 }
 
 bot.on('message', (msg) => {
-	if (msg.content === 'ping') {
-		msg.channel.sendMessage('pong');
-	}
 	if (msg.content === 'who?') {
 		msg.channel.sendFile('images\\chocobocolina.gif');
+	} else {
+		parseCommand(msg);
 	}
 });
 
