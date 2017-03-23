fedddb552e333679ddbabb0ce3596bb9be2a4c4a
Taylor <tsburdette@gmail.com>
2017-03-23 17:56:37 -0400

    Added lol patch note scraping and basic wishlist functionality

--- bot.js
+++ bot.js
@@ -15,11 +15,50 @@ try {
 	process.exit();
 }
 
+var wishlist;
+try {
+	wishlist = require("./wishlist.json");
+} catch(e) {
+	// no wishlist file found
+	wishlist = {};
+}
+
 var commands = {
 	"ping": {
 		description: "responds with 'pong'. Used as a heartbeat command.",
 		process: (bot, msg, suffix) => {
 			msg.channel.sendMessage(msg.author + " pong!");
+		}
+	},
+	"lolpn": {
+		description: "Links the current League of Legends patch notes.",
+		process: (bot, msg, suffix) => {
+			require('./scraper.js').scrape("http://na.leagueoflegends.com/en/tag/patch-notes", {
+					first: "h4 a"
+				}, (err, data) => {
+					msg.channel.sendMessage(err || "The latest League of Legends patch notes can be found here: http://na.leagueoflegends.com" + data.first.prop("href"));
+			});
+		}
+	},
+	"wishlist": {
+		description: "A list of things you would like me to do someday!",
+		process: (bot, msg, suffix) => {
+			console.log("Wishlisting!");
+			wishlist[new Date()] = suffix;
+			console.log("Added to wishlist!");
+			require("fs").writeFile("./wishlist.json", JSON.stringify(wishlist, null, '\t'), null);
+			console.log("Written to file!");
+			msg.channel.sendMessage("Added to wishlist: " + suffix);
+		}
+	},
+	"getWishlist": {
+		description: "Lists out all the wishlist items to date.",
+		process: (bot, msg, suffix) => {
+			var text = "Current wishlist:\n";
+			for (var wish in wishlist) {
+				text += JSON.stringify(wish) + "\n";
+			}
+			msg.channel.sendMessage(text);
 		}
 	}
 }
